7cb532c4a610c685d78912bc378cc63667145cf9
EitchDesign <97712982+EitchDesign@users.noreply.github.com>
2022-07-22 19:19:11 +0900

    Add files via upload

---
+++ home/index.php
@@ -0,0 +1,27 @@
+<?php 
+  require($_SERVER['DOCUMENT_ROOT'].'/_config/helper/load.php');
+
+  $title = 'キャンプとブッシュクラフトの情報サイト';
+  $description = '';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $title ?></title>
+  <meta name="description" content="<?php echo $description; ?>">
+
+  <link rel="canonical" href="" />
+  <link rel="alternate" hreflang="ja" href="" />
+  <link rel="alternate" hreflang="en" href="" />
+</head>
+<body>
+
+<div id="param" class="noload flex">
+
+<p>Hello Warld</p>
+
+</div><!-- #param -->
+
+</body>
+</html>
